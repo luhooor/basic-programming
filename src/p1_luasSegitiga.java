63b28702c1c089a77c293040220017a969369502
luhooor <ylk.osd@gmail.com>
2022-03-03 07:25:22 +0700

    Finalisasi luas segitiga

--- src/p1_luasSegitiga.java
+++ src/p1_luasSegitiga.java
@@ -2,9 +2,9 @@ public class p1_luasSegitiga {
     public static void main(String[] args) {
 //        input
         float alas = 20;
-        float tinggi = 25
+        float tinggi = 25;
 //        kode
         float luas = (alas * tinggi)/2;
-        System.out.println(luas);
+        System.out.println("Luas segitiga dengan alas: " + alas + " dan tinggi: "+ tinggi + " adalah: " + luas);
     }
 }
