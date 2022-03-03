346ae1cadda1220139fcf7065ffac1d4fc89fa23
luhooor <ylk.osd@gmail.com>
2022-03-03 07:22:47 +0700

    membuat rumus luas segitiga

---
+++ src/p1_luasSegitiga.java
@@ -0,0 +1,10 @@
+public class p1_luasSegitiga {
+    public static void main(String[] args) {
+//        input
+        float alas = 20;
+        float tinggi = 25
+//        kode
+        float luas = (alas * tinggi)/2;
+        System.out.println(luas);
+    }
+}
