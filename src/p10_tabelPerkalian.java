a8690ad8eb0d3d14333a139487db948625677b47
luhooor <ylk.osd@gmail.com>
2022-03-04 19:16:12 +0700

    mengubah branching di tabel perkalian

--- src/p10_tabelPerkalian.java
+++ src/p10_tabelPerkalian.java
@@ -1,14 +1,20 @@
 public class p10_tabelPerkalian {
     private static void cetakPerkalian(int n) {
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= n; j++) {
-                System.out.print(i * j + "\t");
+
+        if (n > 30 || n < 1) {
+            System.out.println("Mohon masukkan angka dari 1 - 30");
+        } else {
+
+            for (int i = 1; i <= n; i++) {
+                for (int j = 1; j <= n; j++) {
+                    System.out.print(i * j + "\t");
+                }
+                System.out.println();
             }
-            System.out.println();
         }
     }
 
     public static void main(String[] args) {
-        cetakPerkalian(10);
+        cetakPerkalian(9);
     }
 }
