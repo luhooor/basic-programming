bc7bcb8dd8740e08fc6ee7692a5b4ac8c77e5322
luhooor <ylk.osd@gmail.com>
2022-03-04 15:59:47 +0700

    problem mean done!!!!

---
+++ src/p12_mean.java
@@ -0,0 +1,18 @@
+public class p12_mean {
+    private static float Mean(float[] numbers) {
+        float hasil = 0;
+        float jumlah = 0;
+
+        for (int i = 0; i < numbers.length; i++) {
+            jumlah += numbers[i];
+            hasil = jumlah/numbers.length;
+        }
+        return hasil;
+    }
+
+    public static void main(String[] args) {
+       float[] value = {1,2,3,4};
+
+       System.out.println(Mean(value));
+    }
+}
